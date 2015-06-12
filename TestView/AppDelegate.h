d6828def2261cc46ce3adb44560a1f8e21f5b8eb
Ismael Venegas Castelló <MexLavuDev@MexLavuMac.lan>
2015-06-12 12:50:43 -0500

    Initial Commit

---
+++ TestView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestView
+//
+//  Created by Ismael Venegas Castelló on 12/06/15.
+//  Copyright (c) 2015 Ismael Venegas Castelló. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestView
+//
+//  Created by Ismael Venegas Castelló on 12/06/15.
+//  Copyright (c) 2015 Ismael Venegas Castelló. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
